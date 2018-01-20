9f4cbb26e703a93e06b5e70798b5fdabf992ab1f
swhitaker5 <35543128+swhitaker5@users.noreply.github.com>
2018-01-20 12:54:15 +0100

    Update Week 8_pig latin.py

--- Week 8_pig latin.py
+++ Week 8_pig latin.py
@@ -10,30 +10,25 @@ consonant (or cluster) that appears at the start of the word to the end, then ap
 
 
 
-import re                                                                                   #Import for regular expressions
+import re                                                                                      #Import for regular expressions
 
-mystring = input("Enter anything you would like translated to Pig Latin: \n").lower()       #String input. Converts to lowercase
-split_string = mystring.split()                                                             #splits string by word
+mystring = input("Enter anything you would like translated to Pig Latin: \n").lower()          #String input. Converts to lowercase
+split_string = mystring.split()                                                                #splits string by word
 pl_trans = []
 
-vowel_list = ('a','e','i','o','u','y')                                                      #tuple of vowels
-punc = ('.',',',':',';','!','?'"'",'"')          
-i = 0 
+vowel_list = ('a','e','i','o','u','y')                                                         #tuple of vowels
+punc = ('.',',',':',';','!','?'"'",'"')                                                        #tuple for puncuations
 
-for word in split_string:       
-        if word.startswith(vowel_list):
-        v = re.search(r"(?P<the_word>[\w]+)(?P<punc>([.,!?:;]?))", word)
-        vwl = v.group('the_word') + 'ay'
-        if word.endswith(punc):
-            vwl = vwl + v.group('punc')
-            pl_trans.append(vwl)
-        else:
-            c = re.search(r"(?P<cluster>[^aeiou]+)(?P<remainder>\w+)(?P<punc>([.,!?:;]?))", word)
-            pig_latin = (c.group('remainder') + c.group('cluster') + 'ay')
-            if word.endswith(punc):
-                pig_latin = pig_latin + c.group('punc')
-                pl_trans.append(pig_latin)
+for word in split_string:
+    c = re.search(r"(?P<cluster>[^aeiouy]+)?(?P<remainder>\w+)(?P<punc>([.,!?:;]?))", word)    #re to seperate const and body of word + any puncation
+    if word.startswith(vowel_list):                                                            #combination for words starting w/ vowel
+        pig_latin = (c.group('remainder') + 'ay') 
+    else:                                                                                      #combination for words stating w/ consonants
+        pig_latin = (c.group('remainder') + c.group('cluster') + 'ay')    
+    if word.endswith(punc):                                                                    #If there are any puncuations after the word
+        pig_latin = pig_latin + c.group('punc')
+    pl_trans.append(pig_latin)                                                                 #adds newly created word to a list
        
-print(' '.join(pl_trans))
+print(' '.join(pl_trans))                                                                      #takes the pl_trans list and makes it a string
     
         
