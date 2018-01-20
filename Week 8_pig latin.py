3f4dc41b34c388ec66ce24caf9d5939d3e47406d
swhitaker5 <35543128+swhitaker5@users.noreply.github.com>
2018-01-20 09:38:56 +0100

    Update Week 8_pig latin.py

--- Week 8_pig latin.py
+++ Week 8_pig latin.py
@@ -8,24 +8,31 @@ Write a program to convert text into Pig Latin. This inolves two steps: move any
 consonant (or cluster) that appears at the start of the word to the end, then append ay.
 """
 
-import re
 
-mystring = input("Enter anything you would like translated to Pig Latin: \n").lower()
-split_string = mystring.split()
+
+import re                                                                                   #Import for regular expressions
+
+mystring = input("Enter anything you would like translated to Pig Latin: \n").lower()       #String input. Converts to lowercase
+split_string = mystring.split()                                                             #splits string by word
 pl_trans = []
 
-vowel_list = ('a','e','i','o','u','y')
+vowel_list = ('a','e','i','o','u','y')                                                      #tuple of vowels
 punc = ('.',',',':',';','!','?'"'",'"')          
 i = 0 
 
 for word in split_string:       
-    if word.startswith(vowel_list):
-        vwl = word + 'ay'
-        pl_trans.append(vwl)
-    else:
-        c = re.search(r"(?P<cluster>(^|\s)[^aeiou]+)(?P<remainder>\w+)", word)
-        pig_latin = (c.group('remainder') + c.group('cluster') + 'ay')
-        pl_trans.append(pig_latin)
+        if word.startswith(vowel_list):
+        v = re.search(r"(?P<the_word>[\w]+)(?P<punc>([.,!?:;]?))", word)
+        vwl = v.group('the_word') + 'ay'
+        if word.endswith(punc):
+            vwl = vwl + v.group('punc')
+            pl_trans.append(vwl)
+        else:
+            c = re.search(r"(?P<cluster>[^aeiou]+)(?P<remainder>\w+)(?P<punc>([.,!?:;]?))", word)
+            pig_latin = (c.group('remainder') + c.group('cluster') + 'ay')
+            if word.endswith(punc):
+                pig_latin = pig_latin + c.group('punc')
+                pl_trans.append(pig_latin)
        
 print(' '.join(pl_trans))
     
